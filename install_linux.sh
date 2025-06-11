6b47c521b9670ed365f16cfa8f48d2ee211fde8f
Jake Parker <jparker@CPRIT1406.attlocal.net>
2025-06-11 11:13:30 -0500

    make defang_source/ hidden (.defang_source/) for clarity

--- install_linux.sh
+++ install_linux.sh
@@ -11,15 +11,15 @@ fi
 
 # Copy files to .bin
 echo "[-] Copying files to ~/.bin"
-mkdir ~/.bin/defang_source/
-cp -rf ./* ~/.bin/defang_source/
+mkdir ~/.bin/.defang_source/
+cp -rf ./* ~/.bin/.defang_source/
 echo "[+] Files copied to ~/.bin"
 
 # Make the bash script to run the main file
 echo "[-] Making defang executable..."
 touch ~/.bin/defang
 echo "#!/bin/bash" >> ~/.bin/defang
-echo "~/.bin/defang_source/main.py \$1 \$2" >> ~/.bin/defang
+echo "~/.bin/.defang_source/main.py \$1 \$2" >> ~/.bin/defang
 sudo chmod +x ~/.bin/defang
 
 echo "[+] defang is ready for use"
